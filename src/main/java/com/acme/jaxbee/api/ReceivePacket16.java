9c85d5450f98d6cb9de6a2d9e3d8dfbfeabee83a
John Pauley <pauleyj@gmail.com>
2014-10-22 23:36:28 -0400

    format

--- src/main/java/com/acme/jaxbee/api/ReceivePacket16.java
+++ src/main/java/com/acme/jaxbee/api/ReceivePacket16.java
@@ -23,115 +23,115 @@ import java.util.Arrays;
 
 public class ReceivePacket16 extends RxFrame {
     private static final byte ADDRESS16_LENGTH = 0x02;
-        private static final byte BUFFER_ALLOCATION_CHUNK_SIZE = 0x04;
-        public static final byte FRAME_TYPE = (byte) 0x81;
+    private static final byte BUFFER_ALLOCATION_CHUNK_SIZE = 0x04;
+    public static final byte FRAME_TYPE = (byte) 0x81;
 
-        enum State {
-            ADDRESS16,
-            RSSI,
-            OPTIONS,
-            RECEIVE_DATA,
+    enum State {
+        ADDRESS16,
+        RSSI,
+        OPTIONS,
+        RECEIVE_DATA,
+    }
+
+    private short sourceAddress16;
+    private byte rssi;
+    private byte options;
+    private ByteBuffer data;
+    private State state;
+    private byte index;
+
+    public ReceivePacket16() {
+        sourceAddress16 = Short.MIN_VALUE;
+        rssi = 0x00;
+        options = Byte.MIN_VALUE;
+        data = null;
+        state = State.ADDRESS16;
+    }
+
+    public short getSourceAddress16() {
+        return sourceAddress16;
+    }
+
+    public byte getRssi() {
+        return rssi;
+    }
+
+    public byte getOptions() {
+        return options;
+    }
+
+    public byte[] getData() {
+        byte[] buffer = null;
+        if (data != null) {
+            buffer = new byte[data.position()];
+            data.rewind();
+            data.get(buffer, 0, buffer.length);
         }
+        return buffer;
+    }
 
-        private short sourceAddress16;
-        private byte rssi;
-        private byte options;
-        private ByteBuffer data;
-        private State state;
-        private byte index;
+    State getState() {
+        return state;
+    }
 
-        public ReceivePacket16() {
-            sourceAddress16 = Short.MIN_VALUE;
-            rssi = 0x00;
-            options = Byte.MIN_VALUE;
-            data = null;
-            state = State.ADDRESS16;
+    @Override
+    public byte getFrameType() {
+        return FRAME_TYPE;
+    }
+
+    @Override
+    public void receive(byte b) {
+        if (State.ADDRESS16 == state) {
+            handleStateAddress16(b);
+        } else if (State.RSSI == state) {
+            handleStateRssi(b);
+        } else if (State.OPTIONS == state) {
+            handleStateOptions(b);
+        } else if (State.RECEIVE_DATA == state) {
+            handleStateReceiveData(b);
         }
+    }
 
-        public short getSourceAddress16() {
-            return sourceAddress16;
+    private void handleStateReceiveData(byte b) {
+        if (data == null) {
+            data = ByteBuffer.allocate(BUFFER_ALLOCATION_CHUNK_SIZE);
         }
-
-        public byte getRssi() {
-            return rssi;
+        if (data.position() == data.capacity()) {
+            final byte[] buffer = Arrays.copyOf(data.array(), data.capacity());
+            data = ByteBuffer.allocate(buffer.length + BUFFER_ALLOCATION_CHUNK_SIZE).put(buffer);
         }
+        data.put(b);
+    }
 
-        public byte getOptions() {
-            return options;
-        }
+    private void handleStateOptions(byte b) {
+        options = b;
+        index = 0;
+        state = State.RECEIVE_DATA;
+    }
 
-        public byte[] getData() {
-            byte[] buffer = null;
-            if (data != null) {
-                buffer = new byte[data.position()];
-                data.rewind();
-                data.get(buffer, 0, buffer.length);
-            }
-            return buffer;
-        }
+    private void handleStateRssi(byte b) {
+        rssi = b;
+        index = 0;
+        state = State.OPTIONS;
+    }
 
-        State getState() {
-            return state;
-        }
-
-        @Override
-        public byte getFrameType() {
-            return FRAME_TYPE;
-        }
-
-        @Override
-        public void receive(byte b) {
-            if (State.ADDRESS16 == state) {
-                handleStateAddress16(b);
-            } else if (State.RSSI == state) {
-                handleStateRssi(b);
-            } else if (State.OPTIONS == state) {
-                handleStateOptions(b);
-            } else if (State.RECEIVE_DATA == state) {
-                handleStateReceiveData(b);
-            }
-        }
-
-        private void handleStateReceiveData(byte b) {
-            if (data == null) {
-                data = ByteBuffer.allocate(BUFFER_ALLOCATION_CHUNK_SIZE);
-            }
-            if (data.position() == data.capacity()) {
-                final byte[] buffer = Arrays.copyOf(data.array(), data.capacity());
-                data = ByteBuffer.allocate(buffer.length + BUFFER_ALLOCATION_CHUNK_SIZE).put(buffer);
-            }
-            data.put(b);
-        }
-
-        private void handleStateOptions(byte b) {
-            options = b;
+    private void handleStateAddress16(byte b) {
+        sourceAddress16 = ByteBuffer.allocate(ADDRESS16_LENGTH).putShort(0, sourceAddress16).put(index++, b).getShort();
+        if (index == ADDRESS16_LENGTH) {
             index = 0;
-            state = State.RECEIVE_DATA;
+            state = State.RSSI;
         }
+    }
 
-        private void handleStateRssi(byte b) {
-            rssi = b;
-            index = 0;
-            state = State.OPTIONS;
-        }
-
-        private void handleStateAddress16(byte b) {
-            sourceAddress16 = ByteBuffer.allocate(ADDRESS16_LENGTH).putShort(0, sourceAddress16).put(index++, b).getShort();
-            if (index == ADDRESS16_LENGTH) {
-                index = 0;
-                state = State.RSSI;
-            }
-        }
-
-        @Override
-        public String toString() {
-            return new StringBuffer()
-                .append('{')
-                .append("\"sourceAddress16\" : ").append(String.format("0x%04x", sourceAddress16)).append(", ")
-                .append("\"rssi\" : ").append(String.format("0x%02x", rssi)).append(", ")
-                .append("\"options\" : ").append(options).append(", ")
-                .append("\"data\" : ").append('"').append(new String(getData())).append('"')
-                .append('}')
-                .toString();
-        }
+    @Override
+    public String toString() {
+        return new StringBuffer()
+            .append('{')
+            .append("\"sourceAddress16\" : ").append(String.format("0x%04x", sourceAddress16)).append(", ")
+            .append("\"rssi\" : ").append(String.format("0x%02x", rssi)).append(", ")
+            .append("\"options\" : ").append(options).append(", ")
+            .append("\"data\" : ").append('"').append(new String(getData())).append('"')
+            .append('}')
+            .toString();
+    }
 }
